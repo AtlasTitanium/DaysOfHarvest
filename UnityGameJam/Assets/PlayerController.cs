3bc5ceb2c699a7947ad5b8a11358bbcb27e46cd1
Atlas <AtlasTheTitan77@gmail.com>
2020-02-07 11:52:41 +0100

    3rd person player

---
+++ UnityGameJam/Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 2;
+    public float runSpeed = 6;
+    public float turnSmoothTime = 0.2f;
+    public float speedSmoothTime = 0.1f;
+
+    public float gravity = -12;
+
+    public Transform thirdPersonCamera;
+
+    float turnSmoothVelocity;
+    float speedSmoothVelocity;
+    float currentSpeed;
+    float velocityY;
+    CharacterController controller;
+
+    private void Start() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+
+        if(inputDir != Vector2.zero) {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + thirdPersonCamera.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+        }
+
+        bool running = Input.GetKey(KeyCode.LeftShift);
+        float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        velocityY += Time.deltaTime * gravity;
+        Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (controller.isGrounded) velocityY = 0;
+    }
+}
